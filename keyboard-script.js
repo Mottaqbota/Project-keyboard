f01c087d28c7fb541321f244ee650d2e8c13773f
Motta <125099026+Mottaqbota@users.noreply.github.com>
2023-05-25 14:03:03 -0300

    Update keyboard-script.js

--- keyboard-script.js
+++ keyboard-script.js
@@ -1,21 +1,21 @@
-window.addEventListener('keydown', function(event) {
-    var keycaps = document.getElementsByClassName('keycaps');
-    var capsLock = document.getElementById('capsLock');
-    var keyPressed = event.key.toLowerCase();
-    if (event.getModifierState("CapsLock")) {
-        capsLock.style.display = 'block'
-        console.log("Caps Lock está ativado");
-    } else {
-        capsLock.style.display = 'none'
-        console.log("Caps Lock está desativado");
-    }
+window.addEventListener('keydown', function (event) {
+  var keycaps = document.getElementsByClassName('keycaps');
+  var capsLock = document.getElementById('capsLock');
+  var keyPressed = event.key.toLowerCase();
+  if (event.getModifierState('CapsLock')) {
+    capsLock.style.display = 'block';
+    console.log('Caps Lock está ativado');
+  } else {
+    capsLock.style.display = 'none';
+    console.log('Caps Lock está desativado');
+  }
 
-    Array.from(keycaps).forEach(function(keycap) {
-        if (keycap.textContent.toLowerCase() === keyPressed) {
-            keycap.classList.add('active-keycap');
-            setTimeout(function() {
-                keycap.classList.remove('active-keycap');
-            }, 200);
-        }
-    });
+  Array.from(keycaps).forEach(function (keycap) {
+    if (keycap.textContent.toLowerCase() === keyPressed) {
+      keycap.classList.add('active-keycap');
+      setTimeout(function () {
+        keycap.classList.remove('active-keycap');
+      }, 250);
+    }
+  });
 });
